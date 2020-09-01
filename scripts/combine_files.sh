dddd233b1c98facb2a19d9c9375c3b17e9e6f080
canhnht <canhnht1709@gmail.com>
2020-09-01 22:52:44 +0900

    Add script

--- scripts/combine_files.sh
+++ scripts/combine_files.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-find ~/Downloads/ -type f -print0 | xargs -0 mv -t ~/Videos
+find ~/Downloads/ -type f -print0 | xargs -0 cp -t ~/Videos
 find /home/acworks/Documents/projects/ac-artist/wikiart/wikiart-saved/images/vincent-van-gogh/ -type f -print0 | xargs -0 cp -t /home/acworks/Documents/projects/ac-artist/pytorch-CycleGAN-and-pix2pix/datasets/vincent-van-gogh/trainA/
+
+
+find /path/to/dir -type f -print0 | sort -zR | tail -zn +1001 | xargs -0 rm
