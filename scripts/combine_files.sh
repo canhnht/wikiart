75e306c1b6ca91364638506ff3d6b3bc6e55dc56
Blue Nguyen <thanhcanh@acworks.co.jp>
2020-09-13 20:59:08 +0900

    Add script parse painter by numbers

--- scripts/combine_files.sh
+++ scripts/combine_files.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Ubuntu
 find ~/Downloads/ -type f -print0 | xargs -0 cp -t ~/Videos
 find /home/acworks/Documents/projects/ac-artist/wikiart/wikiart-saved/images/vincent-van-gogh/ -type f -print0 | xargs -0 cp -t /home/acworks/Documents/projects/ac-artist/pytorch-CycleGAN-and-pix2pix/datasets/vincent-van-gogh/trainA/
 
 
-find /path/to/dir -type f -print0 | sort -zR | tail -zn +1001 | xargs -0 rm
+find . -type f -print0 | sort -zR | tail -zn +2001 | xargs -0 rm
+
+
+# Mac OS
+find images/vincent-van-gogh/ -type f -print0 | xargs -0 -I{} cp {} dataset/vincent-van-gogh/
---
+++ scripts/parse_painter_by_numbers.py
@@ -0,0 +1,37 @@
+import os
+from shutil import copyfile
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+ALL_DATA_INFO = '/Volumes/Young Buffalo/datasets/paintings/painter-by-numbers/all_data_info.csv'
+TRAIN_FOLDER = '/Volumes/Young Buffalo/datasets/paintings/painter-by-numbers/train'
+TEST_FOLDER = '/Volumes/Young Buffalo/datasets/paintings/painter-by-numbers/test'
+OUTPUT_FOLDER = '/Users/thanhcanh/Documents/projects/style-transfer/wikiart/wikiart-saved/dataset'
+
+all_data=pd.read_csv(ALL_DATA_INFO, encoding="utf_8")
+print("Original record size", all_data.shape)
+recordClean=all_data.loc[-all_data.isnull().any(axis=1), :]
+recordNaN=all_data.loc[all_data.isnull().any(axis=1), :]
+print("Clean record size", recordClean.shape)
+print("Records with NaN values", recordNaN.shape)
+
+artists = [
+    'jackson pollock',
+    'edvard munch'
+]
+
+for index, row in all_data.iterrows():
+    artist = row['artist'].lower()
+    in_train = row['in_train']
+    folder = row['artist_group']
+    filename = row['new_filename']
+    if artist in artists:
+        if os.path.exists(os.path.join(TRAIN_FOLDER, filename)):
+            filepath = os.path.join(TRAIN_FOLDER, filename)
+        else:
+            filepath = os.path.join(TEST_FOLDER, filename)
+        print('rowwwwwwww', in_train, artist, filepath)
+        artist_folder = os.path.join(OUTPUT_FOLDER, artist)
+        os.makedirs(artist_folder, exist_ok=True)
+        dest = os.path.join(artist_folder, filename)
+        copyfile(filepath, dest)
